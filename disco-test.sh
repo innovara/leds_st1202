6106dc575252e294e7f8141fd3abbfe189135c09
Manuel Fombuena <fombuena@outlook.com>
2025-01-06 10:30:35 +0000

    fix: disco-test.sh: add license and tidy up format
    Signed-off-by: Manuel Fombuena <fombuena@outlook.com>

--- disco-test.sh
+++ disco-test.sh
@@ -1,36 +1,56 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2024, Manuel Fombuena <fombuena@outlook.com>
+#
+# disco-test.sh - Shell script to test LEDs using leds_st1202.sh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 s=0.2s
-for i in $(seq 1 10);do
-./leds_st1202.sh +r
-sleep $s
-./leds_st1202.sh -r
-sleep $s
-./leds_st1202.sh +g
-sleep $s
-./leds_st1202.sh -g
-sleep $s
-./leds_st1202.sh +b
-sleep $s
-./leds_st1202.sh -b
-sleep $s
-./leds_st1202.sh +m
-sleep $s
-./leds_st1202.sh -m
-sleep $s
-./leds_st1202.sh +y
-sleep $s
-./leds_st1202.sh -y
-sleep $s
-./leds_st1202.sh +c
-sleep $s
-./leds_st1202.sh -c
-sleep $s
-./leds_st1202.sh +m
-sleep $s
-./leds_st1202.sh -m
-sleep $s
-./leds_st1202.sh +w
-sleep $s
-./leds_st1202.sh -w
-sleep $s
+
+for i in $(seq 1 10); do
+  ./leds_st1202.sh +r
+  sleep $s
+  ./leds_st1202.sh -r
+  sleep $s
+  ./leds_st1202.sh +g
+  sleep $s
+  ./leds_st1202.sh -g
+  sleep $s
+  ./leds_st1202.sh +b
+  sleep $s
+  ./leds_st1202.sh -b
+  sleep $s
+  ./leds_st1202.sh +m
+  sleep $s
+  ./leds_st1202.sh -m
+  sleep $s
+  ./leds_st1202.sh +y
+  sleep $s
+  ./leds_st1202.sh -y
+  sleep $s
+  ./leds_st1202.sh +c
+  sleep $s
+  ./leds_st1202.sh -c
+  sleep $s
+  ./leds_st1202.sh +m
+  sleep $s
+  ./leds_st1202.sh -m
+  sleep $s
+  ./leds_st1202.sh +w
+  sleep $s
+  ./leds_st1202.sh -w
+  sleep $s
 done
