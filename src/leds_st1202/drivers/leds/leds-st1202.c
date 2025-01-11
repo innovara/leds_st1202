caf6fd454c06ab26ec766faf1a0e6efee82cc5a8
Manuel Fombuena <fombuena@outlook.com>
2025-01-11 23:34:56 +0000

    fix: leds_st1202: NULL pointer access error
    st1202_dt_init() calls devm_led_classdev_register_ext() before the internal data structures are properly setup, so the leds become visible to user space while being partially initialized, leading to a window where trying to access them causes a NULL pointer access. Move devm_led_classdev_register_ext() to the last thing to happen during initialization.
    
    Call st1202_setup() to initialize hardware first and then st1202_dt_init().
    
    Signed-off-by: Manuel Fombuena <fombuena@outlook.com>

--- src/leds_st1202/drivers/leds/leds-st1202.c
+++ src/leds_st1202/drivers/leds/leds-st1202.c
@@ -263,8 +263,6 @@ static int st1202_dt_init(struct st1202_chip *chip)
 	int err, reg;
 
 	for_each_available_child_of_node_scoped(dev_of_node(dev), child) {
-		struct led_init_data init_data = {};
-
 		err = of_property_read_u32(child, "reg", &reg);
 		if (err)
 			return dev_err_probe(dev, err, "Invalid register\n");
@@ -278,15 +276,6 @@ static int st1202_dt_init(struct st1202_chip *chip)
 		led->led_cdev.pattern_set = st1202_led_pattern_set;
 		led->led_cdev.pattern_clear = st1202_led_pattern_clear;
 		led->led_cdev.default_trigger = "pattern";
-
-		init_data.fwnode = led->fwnode;
-		init_data.devicename = "st1202";
-		init_data.default_label = ":";
-
-		err = devm_led_classdev_register_ext(dev, &led->led_cdev, &init_data);
-		if (err < 0)
-			return dev_err_probe(dev, err, "Failed to register LED class device\n");
-
 		led->led_cdev.brightness_set = st1202_brightness_set;
 		led->led_cdev.brightness_get = st1202_brightness_get;
 	}
@@ -349,7 +338,7 @@ static int st1202_probe(struct i2c_client *client)
 {
 	struct st1202_chip *chip;
 	struct st1202_led *led;
-	int ret;
+	int ret,err;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA))
 		return dev_err_probe(&client->dev, -EIO, "SMBUS Byte Data not Supported\n");
@@ -361,15 +350,16 @@ static int st1202_probe(struct i2c_client *client)
 	devm_mutex_init(&client->dev, &chip->lock);
 	chip->client = client;
 
-	ret = st1202_dt_init(chip);
-	if (ret < 0)
-		return ret;
-
 	ret = st1202_setup(chip);
 	if (ret < 0)
 		return ret;
 
+	ret = st1202_dt_init(chip);
+	if (ret < 0)
+		return ret;
+
 	for (int i = 0; i < ST1202_MAX_LEDS; i++) {
+		struct led_init_data init_data = {};
 		led = &chip->leds[i];
 		led->chip = chip;
 		led->led_num = i;
@@ -386,6 +376,14 @@ static int st1202_probe(struct i2c_client *client)
 		if (ret < 0)
 			return dev_err_probe(&client->dev, ret,
 					"Failed to activate LED channel\n");
+
+		init_data.fwnode = led->fwnode;
+		init_data.devicename = "st1202";
+		init_data.default_label = ":";
+
+		err = devm_led_classdev_register_ext(&client->dev, &led->led_cdev, &init_data);
+		if (err < 0)
+			return dev_err_probe(&client->dev, err, "Failed to register LED class device\n");
 	}
 
 	return 0;
